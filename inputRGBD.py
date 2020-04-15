bec74d5af86530a6d2aa3bee8cae26f6d1f679b9
TSAI-CHANCHANG <czz861924@gmail.com>
2020-04-15 15:39:02 +0900

    Add files via upload

---
+++ inputRGBD.py
@@ -0,0 +1,63 @@
+from torch.utils.data.dataset import Dataset, ConcatDataset
+from torch.utils.data.dataloader import DataLoader
+from skimage import io
+import math
+from math import log
+import torch.utils.data as Data
+from torchvision import transforms
+from PIL import Image
+import numpy as np
+
+
+class RGBDDataset(Dataset):
+    def __init__(self, scene_name='', path='', seq='',
+                 frame_num=0):
+        self.scene_name = scene_name
+        self.path = path
+        self.seq = seq
+        self.frame_num = frame_num
+
+    def __getitem__(self, index):
+        if index >= self.frame_num:  # if user input a index larger than max
+            raise IndexError
+        prefix_num = 6 - math.ceil(log(index, 10))
+        temp = self.path + self.scene_name + '/' + self.seq + '/frame-'
+        for i in range(0, prefix_num):
+            temp += '0'
+        this_image_color_path = temp + str(index) + '.color.png'
+        this_image_depth_path = temp + str(index) + '.depth.png'
+        this_frame_pose_path = temp + str(index) + '.pose.txt'
+        # print(this_image_color_path)
+        # print(this_image_depth_path)
+        # print(this_frame_pose_path)
+        img_color = io.imread(this_image_color_path)
+        img_depth = io.imread(this_image_depth_path)
+        frame_pose = np.loadtxt(this_frame_pose_path)
+        # print(img_color)
+        result = (img_color, img_depth, frame_pose)
+        return result
+
+    def __len__(self):
+        return self.frame_num
+
+    def __add__(self, other):
+        return ConcatDataset([self, other])
+
+
+new_dataset = RGBDDataset('office', './', 'seq-01', 1000)
+img_color, img_depth, frame_pose = new_dataset.__getitem__(11)
+print(frame_pose)
+dataloader = DataLoader(new_dataset, batch_size=4, shuffle=True, num_workers=4, drop_last=True)
+# image_color_path = './office/seq-01/seq-01/'
+# index = 200
+# prefix_num = 6 - math.ceil(log(index, 10))
+# print(prefix_num)
+# this_path = image_color_path + 'frame-'
+# for i in range(0, prefix_num):
+#     this_path += '0'
+# this_path += str(index) + '.color.png'
+# print(this_path)
+# img_skimage_color = io.imread('./office/seq-01/seq-01/frame-000000.color.png')
+# img_skimage_depth = io.imread('./office/seq-01/seq-01/frame-000000.depth.png')
+# print('The shape of \n img_skimage_color is {} \n image_skimage_depth is {}'.format(img_skimage_color.shape, img_skimage_depth.shape))
+# print(img_skimage_depth)
